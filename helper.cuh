cae7a1f8b6037f76f013f5fcf960da01ae6a3a13
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 15:18:06 -0800

    save

--- helper.cuh
+++ helper.cuh
@@ -1,9 +1,17 @@
 #pragma once
 
-float *arange(int64_t size) {
-  float *buf = new float[size];
+template <typename T>
+T *arange(int64_t size) {
+  T *buf = new T[size];
   for (int64_t i = 0; i < size; i++) {
-    buf[i] = float(i);
+    buf[i] = T(i);
   }
-  return buf;
+  T *ret;
+  int64_t size_ = size * sizeof(T);
+  cudaMalloc(&ret, size_);
+  cudaMemcpy(ret, buf, size_, cudaMemcpyHostToDevice);
+  cudaDeviceSynchronize();
+  delete [] buf;
+  // who cares about cudaFree :P LOL
+  return ret;
 }
--- main.cu
+++ main.cu
@@ -3,8 +3,15 @@
 
 std::vector<int64_t> shape = {};
 std::vector<std::vector<int64_t>> strides = {};
-std::vector<at::ScalarType> dtypes = {};
-std::vector<char *> data_ptrs = {};
+std::vector<at::ScalarType> dtypes = {
+  at::ScalarType::Float,
+  at::ScalarType::Float,
+  at::ScalarType::Float,
+  at::ScalarType::Float,
+};
+std::vector<char *> data_ptrs = {
+  nullptr, nullptr, nullptr, nullptr
+};
 bool is_contiguous = true;
 int64_t noutputs = 1;
 
@@ -12,6 +19,10 @@ using namespace at;
 using namespace at::native;
 
 int main() {
+  data_ptrs[0] = (char *)arange<float>(30);
+  data_ptrs[1] = (char *)arange<float>(30);
+  data_ptrs[2] = (char *)arange<float>(30);
+  data_ptrs[3] = (char *)arange<float>(30);
   TensorIteratorBase iter;  // uses the hardcoded globals above
   gpu_kernel_multiple_outputs(iter, [] GPU_LAMBDA (float a, float b) {
     return thrust::tuple<float, float>(a + b, a - b);
