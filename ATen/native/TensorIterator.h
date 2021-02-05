f81e7df781b40ce5429b9151233cf6c3fd375d1a
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 14:27:26 -0800

    save

--- ATen/native/TensorIterator.h
+++ ATen/native/TensorIterator.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <cstdint>
+#include <functional>
+#include <numeric>
 #include <c10/core/ScalarType.h>
 
 extern std::vector<int64_t> shape;
@@ -17,6 +19,14 @@ std::vector<int64_t> &shape() {
 
 at::ScalarType dtype(int64_t i) {
   return ::dtypes[i];
+}
+
+int64_t ntensors() {
+  return ::dtypes.size();
+}
+
+int64_t numel() {
+  return std::accumulate(::shape.begin(), ::shape.end(), 1, std::multiplies<int64_t>());
 }
 
 } iter;
