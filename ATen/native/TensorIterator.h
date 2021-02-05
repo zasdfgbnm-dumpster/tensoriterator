2d520c313a47cbf112b7fb83567a003a65d3c9bf
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 14:46:35 -0800

    save

--- ATen/native/TensorIterator.h
+++ ATen/native/TensorIterator.h
@@ -16,51 +16,51 @@ namespace at {
 
 struct TensorIteratorBase {
 
-std::vector<int64_t> &shape() {
+std::vector<int64_t> &shape() const {
   return ::shape;
 }
 
-at::ScalarType dtype(int64_t i) {
+at::ScalarType dtype(int64_t i) const {
   return ::dtypes[i];
 }
 
-int64_t ntensors() {
+int64_t ntensors() const {
   return ::dtypes.size();
 }
 
-int64_t numel() {
+int64_t numel() const {
   return std::accumulate(::shape.begin(), ::shape.end(), 1, std::multiplies<int64_t>());
 }
 
-int64_t ndim() {
+int64_t ndim() const {
   return ::shape.size();
 }
 
-bool can_use_32bit_indexing() {
+bool can_use_32bit_indexing() const {
   return true;
 }
 
-std::array<TensorIteratorBase, 1> with_32bit_indexing() {
+std::array<TensorIteratorBase, 1> with_32bit_indexing() const {
   return {*this};
 }
 
-char *data_ptr(int64_t i) {
+char *data_ptr(int64_t i) const {
   return ::data_ptrs[i];
 }
 
-bool is_contiguous() {
+bool is_contiguous() const {
   return ::is_contiguous;
 }
 
-at::ScalarType input_dtype(int64_t i) {
+at::ScalarType input_dtype(int64_t i) const {
   return ::dtypes[i + ::noutputs];
 }
 
-int64_t noutputs() {
+int64_t noutputs() const {
   return ::noutputs;
 }
 
-int64_t element_size(int64_t i) {
+int64_t element_size(int64_t i) const {
   return c10::elementSize(::dtypes[i]);
 }
 
