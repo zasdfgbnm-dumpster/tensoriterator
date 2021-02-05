6bb1d2b2a23e1ab817f7175882b4175072e9c91b
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 09:44:23 -0800

    save

--- ATen/native/TensorIterator.h
+++ ATen/native/TensorIterator.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <vector>
+#include <cstdint>
+#include <c10/core/ScalarType.h>
+
+extern std::vector<int64_t> shape;
+extern std::vector<at::ScalarType> dtypes;
+
 namespace at {
 
-class TensorIteratorBase {
+struct TensorIteratorBase {
 
+std::vector<int64_t> &shape() {
+  return ::shape;
+}
 
+at::ScalarType dtype(int64_t i) {
+  return ::dtypes[i];
+}
 
-};
+} iter;
 
 }
--- main.cu
+++ main.cu
@@ -1,4 +1,13 @@
 #include <ATen/native/cuda/Loops.cuh>
 
+std::vector<int64_t> shape = {};
+std::vector<at::ScalarType> dtypes = {};
+
+using namespace at;
+using namespace at::native;
+
 int main() {
+  gpu_kernel(iter, [] GPU_LAMBDA (float a, float b) {
+    return a + b;
+  });
 }
