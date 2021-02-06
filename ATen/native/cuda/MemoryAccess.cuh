256ba1e457e87858f1bc7c48d80c215e8283e430
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 22:45:00 -0800

    save

--- ATen/native/cuda/MemoryAccess.cuh
+++ ATen/native/cuda/MemoryAccess.cuh
@@ -27,11 +27,8 @@ struct unroll {
 
 template <typename data_t, typename out_calc_t>
 struct multi_outputs_unroll : unroll<out_calc_t, LoadWithoutCast> {
-  data_t data;
-
   __device__ multi_outputs_unroll(data_t data, out_calc_t oc):
-    unroll<out_calc_t, LoadWithoutCast>(oc, LoadWithoutCast()),
-    data(data) {}
+    unroll<out_calc_t, LoadWithoutCast>(oc, LoadWithoutCast()) {}
 
   __device__ inline offset_t offsets(int linear_idx) {
     return this->output_offset_calculator.get(linear_idx);
