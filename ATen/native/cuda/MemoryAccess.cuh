a9657321fe1039c9c6e4840ee7415fb89f00509a
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 21:44:27 -0800

    save

--- ATen/native/cuda/MemoryAccess.cuh
+++ ATen/native/cuda/MemoryAccess.cuh
@@ -18,22 +18,22 @@ namespace policies {
 
 // Assumption:
 // all tensors are contiguous, that is: stride == sizeof(type) for all tensors
-template<typename inp_calc_t, typename out_calc_t, typename loader_t>
+template<typename out_calc_t, typename loader_t>
 struct unroll {
-  inp_calc_t input_offset_calculator;
   out_calc_t output_offset_calculator;
 
-  __device__ unroll(inp_calc_t ic, out_calc_t oc, loader_t l):
-    input_offset_calculator(ic), output_offset_calculator(oc) {}
+  __device__ unroll(out_calc_t oc, loader_t l): output_offset_calculator(oc) {}
 };
 
 template <typename data_t, typename inp_calc_t, typename out_calc_t>
-struct multi_outputs_unroll : unroll<inp_calc_t, out_calc_t, LoadWithoutCast> {
+struct multi_outputs_unroll : unroll<out_calc_t, LoadWithoutCast> {
   data_t data;
   int remaining;
+  inp_calc_t input_offset_calculator;
 
   __device__ multi_outputs_unroll(data_t data, int remaining, inp_calc_t ic, out_calc_t oc):
-    unroll<inp_calc_t, out_calc_t, LoadWithoutCast>(ic, oc, LoadWithoutCast()), data(data), remaining(remaining) {}
+    unroll<out_calc_t, LoadWithoutCast>(oc, LoadWithoutCast()),
+    data(data), remaining(remaining), input_offset_calculator(ic) {}
 
   __device__ inline bool check_inbounds(int thread_work_elem) {
     return ((threadIdx.x  + thread_work_elem*num_threads) < this->remaining);
