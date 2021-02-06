ed9468e04eb5428d7735b6efcb15dcf5189f13ff
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 16:56:21 -0800

    save

--- main.cu
+++ main.cu
@@ -1,9 +1,4 @@
-#include <vector>
 #include <iostream>
-
-std::vector<char *> data_ptrs = {
-  nullptr, nullptr
-};
 
 template <typename T>
 T *arange(int64_t size) {
@@ -14,7 +9,8 @@ T *arange(int64_t size) {
   T *ret;
   int64_t size_ = size * sizeof(T);
   cudaMalloc(&ret, size_);
-  cudaMemcpy(ret, buf, size_, cudaMemcpyHostToDevice);
+  cudaDeviceSynchronize();
+  cudaMemcpy(ret, buf, size_, cudaMemcpyDefault);
   cudaDeviceSynchronize();
   delete [] buf;
   // who cares about cudaFree :P LOL
@@ -26,7 +22,7 @@ void print(T *data, int64_t size) {
   T *buf = new T[size];
   int64_t size_ = size * sizeof(T);
   cudaDeviceSynchronize();
-  cudaMemcpy(buf, data, size_, cudaMemcpyDeviceToHost);
+  cudaMemcpy(buf, data, size_, cudaMemcpyDefault);
   cudaDeviceSynchronize();
   for (int64_t i = 0; i < size; i++) {
     std::cout << buf[i] << ", ";
@@ -36,9 +32,7 @@ void print(T *data, int64_t size) {
 }
 
 int main() {
-  data_ptrs[0] = (char *)arange<float>(30);
-  data_ptrs[1] = (char *)arange<float>(30);
+  float *a = arange<float>(30);
   cudaDeviceSynchronize();
-  print((float *)data_ptrs[0], 30);
-  print((float *)data_ptrs[1], 30);
+  print(a, 30);
 }
