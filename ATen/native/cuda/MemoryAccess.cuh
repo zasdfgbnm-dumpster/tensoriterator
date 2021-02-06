734db0f2ed768882faa1d94c78c31ec4cbd954cb
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 22:45:38 -0800

    save

--- ATen/native/cuda/MemoryAccess.cuh
+++ ATen/native/cuda/MemoryAccess.cuh
@@ -7,17 +7,12 @@
 
 #include <thrust/tuple.h>
 
-// References:
-// https://devblogs.nvidia.com/cuda-pro-tip-increase-performance-with-vectorized-memory-access/
-
 namespace at { namespace native { namespace memory {
 
 struct LoadWithoutCast {};
 
 namespace policies {
 
-// Assumption:
-// all tensors are contiguous, that is: stride == sizeof(type) for all tensors
 template<typename out_calc_t, typename loader_t>
 struct unroll {
   out_calc_t output_offset_calculator;
