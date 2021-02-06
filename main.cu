813aaa0050ae82d30b998d8dec46e2dcf2ce5c30
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 17:00:08 -0800

    save

--- main.cu
+++ main.cu
@@ -1,28 +1,25 @@
 #include <iostream>
 
 template <typename T>
-T *arange(int64_t size) {
+void echo_arange(int64_t size) {
+  int64_t size_ = size * sizeof(T);
+
+  // set
   T *buf = new T[size];
   for (int64_t i = 0; i < size; i++) {
     buf[i] = T(i);
   }
-  T *ret;
-  int64_t size_ = size * sizeof(T);
-  cudaMalloc(&ret, size_);
+  T *dev;
+  cudaMalloc(&dev, size_);
   cudaDeviceSynchronize();
-  cudaMemcpy(ret, buf, size_, cudaMemcpyDefault);
+  cudaMemcpy(dev, buf, size_, cudaMemcpyDefault);
   cudaDeviceSynchronize();
   delete [] buf;
-  // who cares about cudaFree :P LOL
-  return ret;
-}
 
-template <typename T>
-void print(T *data, int64_t size) {
-  T *buf = new T[size];
-  int64_t size_ = size * sizeof(T);
+  // print
+  buf = new T[size];
   cudaDeviceSynchronize();
-  cudaMemcpy(buf, data, size_, cudaMemcpyDefault);
+  cudaMemcpy(buf, dev, size_, cudaMemcpyDefault);
   cudaDeviceSynchronize();
   for (int64_t i = 0; i < size; i++) {
     std::cout << buf[i] << ", ";
@@ -32,7 +29,5 @@ void print(T *data, int64_t size) {
 }
 
 int main() {
-  float *a = arange<float>(30);
-  cudaDeviceSynchronize();
-  print(a, 30);
+  echo_arange<float>(30);
 }
