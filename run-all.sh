244628b85313ddeca5a47b927faf9a5565d65113
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 16:10:15 -0800

    save

--- run-all.sh
+++ run-all.sh
@@ -6,9 +6,9 @@ echo 10.2
 docker run -v $PWD:/w -w /w -it --entrypoint "/bin/bash" nvcr.io/nvidia/pytorch:19.12-py3 ./build.sh
 echo ""
 
-# echo 10.2
-# docker run -v $PWD:/w -w /w -it --entrypoint "/bin/bash" nvcr.io/nvidia/pytorch:19.12-py3 ./build.sh
-# echo ""
+echo 11.0
+docker run -v $PWD:/w -w /w -it --entrypoint "/bin/bash" nvcr.io/nvidia/pytorch:20.07-py3 ./build.sh
+echo ""
 
 echo 11.1
 docker run -v $PWD:/w -w /w -it --entrypoint "/bin/bash" nvcr.io/nvidia/pytorch:20.12-py3 ./build.sh
