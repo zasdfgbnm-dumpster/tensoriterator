29734584ca0e0e001e0fa6df5bc87d4a41723f12
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 21:12:50 -0800

    save

--- ATen/native/cuda/MemoryAccess.cuh
+++ ATen/native/cuda/MemoryAccess.cuh
@@ -41,13 +41,6 @@ struct StoreWithoutCast {
   }
 };
 
-
-// aligned vector generates vectorized load/store on CUDA
-template<typename scalar_t, int vec_size>
-struct alignas(sizeof(scalar_t) * vec_size) aligned_vector {
-  scalar_t val[vec_size];
-};
-
 namespace policies {
 
 // Assumption:
