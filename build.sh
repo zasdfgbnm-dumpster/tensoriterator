49dee5efb88735712a20ecbd9f6fcaf2072ea2ee
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-05 18:13:10 -0800

    save

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-nvcc main.cu -o test -I. -std=c++14 --extended-lambda --expt-relaxed-constexpr
-./test
-rm -rf test
+nvcc add.cu -o add -I. -std=c++14 --extended-lambda --expt-relaxed-constexpr
+nvcc add-subtract.cu -o add-subtract -I. -std=c++14 --extended-lambda --expt-relaxed-constexpr
+./add
+echo ""
+./add-subtract
+echo ""
+rm -rf add add-subtract
