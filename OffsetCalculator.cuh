ab02f933c28284fd3eb54f46f50c4d1f2d9c3788
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-06 00:15:41 -0800

    save

--- OffsetCalculator.cuh
+++ OffsetCalculator.cuh
@@ -13,20 +13,18 @@ using offset_t = at::detail::Array<uint32_t, std::max<int>(2, 1)>;
 struct OffsetCalculator {
   OffsetCalculator(int dims) : dims(dims) {}
 
-  __host__ __device__ offset_t get(index_t linear_idx) const {
-    offset_t offsets;
-    offsets[0] = 0;
-    offsets[1] = 0;
+  __host__ __device__ int get(index_t linear_idx) const {
+    int offset;
+    offset = 0;
 
     #pragma unroll
     for (int dim = 0; dim < MAX_DIMS; ++dim) {
       if (dim == dims) {
         break;
       }
-      offsets[0] = linear_idx;
-      offsets[1] = linear_idx;
+      offset = linear_idx;
     }
-    return offsets;
+    return offset;
   }
 
   int dims;
