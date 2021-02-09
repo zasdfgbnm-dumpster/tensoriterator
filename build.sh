6ca193ef440241082c9d08fa0b7c8be3303cba63
mcarilli <mcarilli@gmail.com>
2021-02-09 15:43:18 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-nvcc add.cu -o add -I. -std=c++14 --extended-lambda --expt-relaxed-constexpr
+nvcc add.cu -o add -I. -std=c++14 --extended-lambda --expt-relaxed-constexpr --resource-usage
 ./add
 echo ""
 # rm -rf add
