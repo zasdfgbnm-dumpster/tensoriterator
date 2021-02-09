1a456af6bb988d23e6f210cd74ebe068ff73c4d9
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-02-09 12:28:32 -0800

    save

--- where.cu
+++ where.cu
@@ -2,14 +2,14 @@
 #include <helper.cuh>
 
 std::vector<int64_t> shape = {
-  5, 5, 5
+  225
 };
 std::vector<std::vector<int64_t>> strides = {
   // warning: strides are in bytes!
-  {16, 80, 400},
-  {1, 5, 25},
-  {16, 80, 400},
-  {0, 0, 0},
+  {16},
+  {1},
+  {16},
+  {0},
 };
 std::vector<at::ScalarType> dtypes = {
   at::ScalarType::ComplexDouble,
@@ -27,13 +27,13 @@ using namespace at;
 using namespace at::native;
 
 int main() {
-  data_ptrs[0] = (char *)zeros<c10::complex<double>>(30);
-  data_ptrs[1] = (char *)zeros<bool>(30);
-  data_ptrs[2] = (char *)arange<c10::complex<double>>(30);
+  data_ptrs[0] = (char *)zeros<c10::complex<double>>(225);
+  data_ptrs[1] = (char *)zeros<bool>(225);
+  data_ptrs[2] = (char *)arange<c10::complex<double>>(225);
   data_ptrs[3] = (char *)arange<c10::complex<double>>(1);
-  print((bool *)data_ptrs[1], 30);
-  print((c10::complex<double> *)data_ptrs[2], 30);
-  print((c10::complex<double> *)data_ptrs[3], 30);
+  print((bool *)data_ptrs[1], 225);
+  print((c10::complex<double> *)data_ptrs[2], 225);
+  print((c10::complex<double> *)data_ptrs[3], 1);
   cudaDeviceSynchronize();
   TensorIteratorBase iter;  // uses the hardcoded globals above
   gpu_kernel(iter, [] GPU_LAMBDA (bool cond, c10::complex<double> a, c10::complex<double> b) {
