37e00623d448494e46df802ff4a8e2caf9b023aa
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-02-09 15:37:21 -0800

    save

--- ATen/native/cuda/MemoryAccess.cuh
+++ ATen/native/cuda/MemoryAccess.cuh
@@ -99,45 +99,10 @@ struct LoadWithoutCast {
   }
 };
 
-template <int N>
-struct LoadWithCast {
-  using array_t = at::detail::Array<at::ScalarType, std::max<int>(N, 1)>;
-  using size_array_t = at::detail::Array<uint32_t, std::max<int>(N, 1)>;
-
-  array_t dtypes;
-  size_array_t element_sizes;
-
-  template<typename array_t_>
-  LoadWithCast(array_t_ dtypes) {
-    #pragma unroll
-    for (int i = 0; i < N; i++) {
-      this->dtypes[i] = dtypes[i];
-      element_sizes[i] = c10::elementSize(dtypes[i]);
-    }
-  }
-
-  template<typename scalar_t>
-  __device__ scalar_t load(char *base_ptr, uint32_t offset, int arg) {
-    void *ptr = base_ptr + element_sizes[arg] * offset;
-    return c10::fetch_and_cast<scalar_t>(dtypes[arg], ptr);
-  }
-};
-
 struct StoreWithoutCast {
   template<typename scalar_t>
   __device__ void store(scalar_t value, char *base_ptr, uint32_t offset) {
     *(reinterpret_cast<scalar_t *>(base_ptr) + offset) = value;
-  }
-};
-
-struct StoreWithCast {
-  at::ScalarType dtype;
-  uint32_t element_size;
-  StoreWithCast(at::ScalarType dtype): dtype(dtype), element_size(c10::elementSize(dtype)) {}
-  template<typename scalar_t>
-  __device__ void store(scalar_t value, char *base_ptr, uint32_t offset) {
-    void *ptr = base_ptr + element_size * offset;
-    c10::cast_and_store<scalar_t>(dtype, ptr, value);
   }
 };
 
@@ -201,126 +166,6 @@ struct unroll {
   }
 };
 
-// Assumption:
-// all tensors are contiguous, that is: stride == sizeof(type) for all tensors
-// Note:
-// Functions in vectorized policy does not do boundary check. It assumes the whole block
-// has its job to do. So the reminders should be handled by the the caller manually.
-template <int vec_size, typename data_t>  // vec_size: number of scalars, can be 1, 2, or 4.
-struct vectorized {
-
-  static_assert(thread_work_size % vec_size == 0, "The workload per thread must be a multiple of vec_size");
-  static constexpr int loop_size = thread_work_size / vec_size;
-
-  data_t data;
-
-  __device__ vectorized(data_t data) : data(data) {}
-
-  __device__ inline constexpr bool check_inbounds(int thread_work_elem) {
-    return true;
-  }
-
-  template<typename accessor_t, typename scalar_t>
-  __device__ inline void load_single_arg(accessor_t to, scalar_t *from) {
-    using vec_t = aligned_vector<scalar_t, vec_size>;
-    vec_t *from_ = reinterpret_cast<vec_t *>(from);
-    int thread_idx = threadIdx.x;
-    #pragma unroll
-    for (int i = 0; i < loop_size; i++) {
-      int index = thread_idx + i * num_threads;
-      vec_t v = from_[index];
-      #pragma unroll
-      for (int j = 0; j < vec_size; j++) {
-        to(vec_size * i + j) = v.val[j];
-      }
-    }
-  }
-
-  template<typename args_t>
-  __device__ inline void load(args_t *args, int idx) {
-    constexpr int arity = std::tuple_size<args_t>::value;
-    detail::static_unroll<detail::vectorized_load_helper, arity>::with_args(*this, args, idx);
-  }
-
-  template<typename scalar_t>
-  __device__ inline void store(scalar_t *from, int idx) {
-    using vec_t = aligned_vector<scalar_t, vec_size>;
-    scalar_t *to = reinterpret_cast<scalar_t *>(data[0]) + block_work_size * idx;
-    vec_t *to_ = reinterpret_cast<vec_t *>(to);
-    int thread_idx = threadIdx.x;
-    #pragma unroll
-    for (int i = 0; i < loop_size; i++) {
-      int index = thread_idx + i * num_threads;
-      vec_t v;
-      for (int j = 0; j < vec_size; j++) {
-        v.val[j] = from[vec_size * i + j];
-      }
-      to_[index] = v;
-    }
-  }
-};
-
-template <typename data_t, typename inp_calc_t, typename out_calc_t, int num_outputs>
-struct multi_outputs_unroll : unroll<data_t, inp_calc_t, out_calc_t, LoadWithoutCast, StoreWithoutCast, num_outputs> {
-
-  __device__ multi_outputs_unroll(data_t data, int remaining, inp_calc_t ic, out_calc_t oc):
-    unroll<data_t, inp_calc_t, out_calc_t, LoadWithoutCast, StoreWithoutCast, num_outputs>(data, remaining, ic, oc, LoadWithoutCast(), StoreWithoutCast()) {}
-
-  template <typename return_t>
-  __device__ inline void store(return_t *from, int idx) {
-    int thread_idx = threadIdx.x;
-    #pragma unroll
-    for (int i = 0; i < thread_work_size; i++) {
-      if (thread_idx >= this->remaining) {
-        return;
-      }
-      int linear_idx = thread_idx + block_work_size * idx;
-      auto offsets = this->output_offset_calculator.get(linear_idx);
-      memory::detail::static_unroll<detail::multi_outputs_store_helper, num_outputs>::with_args(this->data, offsets, from[i]);
-      thread_idx += num_threads;
-    }
-  }
-};
-
 }  // namespace policies
-
-// This is only used in host, but we will wrap this into some templates
-// which is C10_HOST_DEVICE, so we have to make this C10_HOST_DEVICE
-// in order to compile
-template<typename scalar_t>
-inline C10_HOST_DEVICE int can_vectorize_up_to(char *pointer) {
-  uint64_t address = reinterpret_cast<uint64_t>(pointer);
-  constexpr int vec2_alignment = std::alignment_of<aligned_vector<scalar_t, 2>>::value;
-  constexpr int vec4_alignment = std::alignment_of<aligned_vector<scalar_t, 4>>::value;
-  if (address % vec4_alignment == 0) {
-    return 4;
-  } else if (address % vec2_alignment == 0) {
-    return 2;
-  }
-  return 1;
-}
-
-template<int i>
-struct can_vectorize_up_to_helper {
-  template <typename array_t, typename traits>
-  static C10_HOST_DEVICE void apply(int &result, array_t pointers, traits _) {
-    using arg_t = typename traits::template arg<i>::type;
-    // `pointers` hold the data_ptr for tensors [output, input0, input1, ...], so we
-    // need a +1 offset to get the input
-    result = std::min<int>(result, can_vectorize_up_to<arg_t>(pointers[i + 1]));
-  }
-};
-
-template<typename func_t, typename array_t>
-inline int can_vectorize_up_to(array_t pointers) {
-  using traits = function_traits<func_t>;
-  using return_t = typename traits::result_type;
-  constexpr int arity = traits::arity;
-  int result = can_vectorize_up_to<return_t>(pointers[0]);
-  // We need to get the type for each argument of `func_t`, this can only
-  // be done at compile time.
-  detail::static_unroll<can_vectorize_up_to_helper, arity>::with_args(result, pointers, traits());
-  return result;
-}
 
 }}} // namespace at::native::memory
