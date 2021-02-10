6b28fc3e2ed7c4cf5b0541fbd9d9e41d520f43ad
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-02-09 17:22:21 -0800

    save

--- where.cu
+++ where.cu
@@ -6,7 +6,7 @@ struct alignas(16) A {
 };
 
 template<int arg_index>
-struct unroll_load_helper {
+struct initialize {
   template <typename args_t>
   static __device__ void apply(args_t *args, int j) {
     printf("%d%d\n", 0, 0);
@@ -14,11 +14,11 @@ struct unroll_load_helper {
   }
 };
 
-struct static_unroll {
+struct initialize_all {
   template<typename... Args>
   static inline __host__ __device__ void with_args(Args&&... args) {
-    unroll_load_helper<0>::apply(std::forward<Args>(args)...);
-    unroll_load_helper<1>::apply(std::forward<Args>(args)...);
+    initialize<0>::apply(std::forward<Args>(args)...);
+    initialize<1>::apply(std::forward<Args>(args)...);
   }
 };
 
@@ -27,7 +27,7 @@ __global__ void unrolled_elementwise_kernel(A *result)
   std::tuple<bool, A> args[2];
   #pragma unroll
   for (int i = 0; i < 2; i++) {
-    static_unroll::with_args(args, i);
+    initialize_all::with_args(args, i);
   }
 
   if ((int)blockIdx.x >= 0) {
