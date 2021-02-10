fbca24d4d3e6239a34d9e5d1b4354869319f2275
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-02-09 16:58:33 -0800

    save

--- where.cu
+++ where.cu
@@ -7,19 +7,14 @@ struct alignas(16) A {
   double data[2];
 };
 
-template<template<int i> typename func, int end, int current=0>
+template<template<int i> typename func>
 struct static_unroll {
   template<typename... Args>
   static inline __host__ __device__ void with_args(Args&&... args) {
-    func<current>::apply(std::forward<Args>(args)...);
-    static_unroll<func, end, current+1>::with_args(args...);
+    func<0>::apply(std::forward<Args>(args)...);
+    func<1>::apply(std::forward<Args>(args)...);
+    func<2>::apply(std::forward<Args>(args)...);
   }
-};
-
-template<template<int i> typename func, int end>
-struct static_unroll<func, end, end> {
-  template<typename... Args>
-  static inline __host__ __device__ void with_args(Args... args) {}
 };
 
 // helper structs to be used with static_unroll to load arguments
@@ -49,7 +44,7 @@ struct unroll {
     constexpr int arity = std::tuple_size<args_t>::value;
     #pragma unroll
     for (int i = 0; i < thread_work_size; i++) {
-      static_unroll<unroll_load_helper, arity>::with_args(*this, args, i);
+      static_unroll<unroll_load_helper>::with_args(*this, args, i);
     }
   }
 };
