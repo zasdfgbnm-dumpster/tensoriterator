f14da16875701fcf3add522bc0564a3770c4c197
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2021-02-11 14:20:59 -0800

    gencode 80, 70

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-nvcc add.cu -o add -I. -std=c++14 --extended-lambda --expt-relaxed-constexpr --resource-usage
+nvcc add.cu -o add -I. -std=c++14 \
+    --extended-lambda \
+    --expt-relaxed-constexpr \
+    --resource-usage \
+    -gencode=arch=compute_80,code=sm_80 \
+    -gencode=arch=compute_70,code=sm_70
+
 ./add
 echo ""
 # rm -rf add
